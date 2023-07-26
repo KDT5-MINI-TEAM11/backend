0e616e9539f03151a46403460910aeb4da54f770
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-26 14:21:54 +0900

    feat [KDT5-MINI-TEAM11]#4 : 회원가입 기능 구현 및 SecurityConfig-PasswordEncoder 추가

--- src/main/java/fastcampus/scheduling/user/user/service/UserService.java
+++ src/main/java/fastcampus/scheduling/user/user/service/UserService.java
@@ -1,8 +1,28 @@
 package fastcampus.scheduling.user.user.service;
 
+import fastcampus.scheduling.user.dto.UserRequest;
+import fastcampus.scheduling.user.dto.UserResponse;
+import fastcampus.scheduling.user.user.model.User;
+import fastcampus.scheduling.user.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserResponse.JoinDTO save(UserRequest.JoinDTO joinDTO){
+//        if(joinDTO == null)
+//            throw new
+        User user = joinDTO.toEntityWithHashPassword(passwordEncoder);
+        User persistencUser = userRepository.save(user);
+        return UserResponse.JoinDTO.from(persistencUser);
+    }
 
 }
