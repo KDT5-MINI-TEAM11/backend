5f2cfd73d32dd794d6d0c14ff3aeb39d780e094a
jongy <jongyoon+u@gmail.com>
2023-07-27 19:43:35 +0900

    feat #2 : modify User model
    set userPassword Maximum length to 100

--- src/main/java/fastcampus/scheduling/user/user/model/User.java
+++ src/main/java/fastcampus/scheduling/user/user/model/User.java
@@ -35,7 +35,7 @@ public class User {
     @Column(unique = true, nullable = false, length = 100)
     private String userEmail;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 100)
     private String userPassword;
 
     @Column(nullable = false, length = 50)
