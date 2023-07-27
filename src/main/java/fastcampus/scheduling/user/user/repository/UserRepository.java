0ced85f797b3fe939a0d6d110357cf9742887241
jongy <jongyoon+u@gmail.com>
2023-07-27 19:44:56 +0900

    feat #2 : impl UserRepository
    impl UserRepository

--- src/main/java/fastcampus/scheduling/user/user/repository/UserRepository.java
+++ src/main/java/fastcampus/scheduling/user/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package fastcampus.scheduling.user.user.repository;
 
 import fastcampus.scheduling.user.user.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUserEmail(String email);
 
 }
