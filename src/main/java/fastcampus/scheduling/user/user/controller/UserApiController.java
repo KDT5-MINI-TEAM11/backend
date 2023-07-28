f81447d096f406a4a9edf40c5190c45de77bc13a
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-28 17:02:14 +0900

    feat [KDT5-MINI-TEAM11]#4 : 유저Api컨트롤러 메서드 추가
    -Endpoint변경
    -join -> singUp 이름 변경
    -checkPhoneNumber 추가

--- src/main/java/fastcampus/scheduling/user/user/controller/UserApiController.java
+++ src/main/java/fastcampus/scheduling/user/user/controller/UserApiController.java
@@ -1,10 +1,11 @@
 package fastcampus.scheduling.user.user.controller;
 
 import fastcampus.scheduling._core.util.ApiResponse;
-import fastcampus.scheduling.user.dto.UserRequest;
-import fastcampus.scheduling.user.dto.UserResponse;
+import fastcampus.scheduling.user.user.dto.UserRequest;
+import fastcampus.scheduling.user.user.dto.UserRequest.CheckPhoneDTO;
+import fastcampus.scheduling.user.user.dto.UserRequest.SignUpDTO;
+import fastcampus.scheduling.user.user.dto.UserResponse;
 import fastcampus.scheduling.user.user.service.UserService;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +21,20 @@ public class UserApiController {
 
     private final UserService userService;
 
-    @PostMapping("/api/join")
-    public ResponseEntity<ApiResponse.Result<UserResponse.JoinDTO>> join(
-        @RequestBody @Valid UserRequest.JoinDTO joinDTO, Errors errors) {
+    @PostMapping("/api/v1/auth/signup")
+    public ResponseEntity<ApiResponse.Result<UserResponse.SignUpDTO>> signUp(@RequestBody UserRequest.SignUpDTO signUpDTO, Errors errors) {
 
-        log.info("/user/join POST " + joinDTO);
+        log.info("/api/user/signup POST " + signUpDTO);
+        UserResponse.SignUpDTO signUpResponse = userService.save(signUpDTO); // 중복등은 서비스에서 체크
 
-        UserResponse.JoinDTO joinResponse = userService.save(joinDTO);
-        return ResponseEntity.ok(ApiResponse.success(joinResponse));
+        return ResponseEntity.ok(ApiResponse.success(signUpResponse));
+    }
+
+    @PostMapping("/api/v1/auth/checkPhoneNumber")
+    public ResponseEntity<ApiResponse.Result<Object>> checkPhoneNumber(@RequestBody CheckPhoneDTO checkPhoneDTO, Errors erros) {
+        log.info("/api/v1/auth/checkPhoneNumber POST " + checkPhoneDTO);
+        userService.checkPhone(checkPhoneDTO);
+
+        return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
