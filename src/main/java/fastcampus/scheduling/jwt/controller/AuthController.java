cef9010c7c836d06dad6d798deb1036fbcd40ea7
jongy <jongyoon+u@gmail.com>
2023-07-28 23:17:33 +0900

    feat #2 : Modify AuthController
    return proper type for response

--- src/main/java/fastcampus/scheduling/jwt/controller/AuthController.java
+++ src/main/java/fastcampus/scheduling/jwt/controller/AuthController.java
@@ -2,7 +2,7 @@ package fastcampus.scheduling.jwt.controller;
 
 import fastcampus.scheduling._core.util.ApiResponse;
 import fastcampus.scheduling._core.util.CookieProvider;
-import fastcampus.scheduling.jwt.dto.JwtTokenDto;
+import fastcampus.scheduling.jwt.dto.RefreshAccessTokenDto;
 import fastcampus.scheduling.jwt.service.RefreshTokenService;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,27 +23,14 @@ public class AuthController {
 	private final CookieProvider cookieProvider;
 
 	@GetMapping("/v1/auth/refresh-token")
-	public ResponseEntity<ApiResponse.Result<String>> refreshToken(HttpServletRequest request) {
+	public ResponseEntity<ApiResponse.Result<RefreshAccessTokenDto>> refreshToken(HttpServletRequest request) {
 		String refreshToken = cookieProvider.getRefreshToken(request);
-		JwtTokenDto jwtTokenDto = refreshTokenService.refreshAccessToken(refreshToken);
+		RefreshAccessTokenDto refreshAccessTokenDto = refreshTokenService.refreshAccessToken(refreshToken);
 
 		ResponseCookie responseCookie = cookieProvider.generateRefreshTokenCookie(refreshToken);
 		return ResponseEntity.status(HttpStatus.OK)
 				.header(HttpHeaders.SET_COOKIE, responseCookie.toString())
-				.body(ApiResponse.success(jwtTokenDto.getAccessToken()));
-	}
-
-	@PostMapping("/v1/auth/signout")
-	public ResponseEntity<ApiResponse.Result<String>> logout(@RequestHeader(name = "Authorization") HttpServletRequest request) {
-		String refreshToken = cookieProvider.getRefreshToken(request);
-		String userId = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-		refreshTokenService.revokeToken(userId, refreshToken);
-
-		ResponseCookie refreshCookie = cookieProvider.removeRefreshTokenCookie();
-
-		return ResponseEntity.status(HttpStatus.OK)
-				.header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
-				.body(ApiResponse.success("로그아웃 완료"));
+				.body(ApiResponse.success(refreshAccessTokenDto));
 	}
 
 }
