2889922eed07c554abcb3ab7d522b5fd303f753f
jongy <jongyoon+u@gmail.com>
2023-07-28 23:42:35 +0900

    feat #2 : resolve conflict

--- src/main/java/fastcampus/scheduling/user/controller/UserController.java
+++ src/main/java/fastcampus/scheduling/user/controller/UserController.java
@@ -4,11 +4,13 @@ import fastcampus.scheduling._core.util.ApiResponse;
 import fastcampus.scheduling.user.dto.UserRequest;
 import fastcampus.scheduling.user.dto.UserResponse;
 import fastcampus.scheduling.user.dto.UserResponse.GetMyPageDTO;
+import fastcampus.scheduling.user.dto.UserResponse.GetUserHeaderDTO;
 import fastcampus.scheduling.user.model.User;
 import fastcampus.scheduling.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +23,21 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/api/v1/user/userHeader")
+    public ResponseEntity<ApiResponse.Result<GetUserHeaderDTO>> getUserHead() {
+        log.info("/api/v1/user/userHeader GET ");
+
+        Long userId = Long.valueOf(
+						SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+        User user = userService.getUserById(userId);
+
+        int totalVacation = user.getPosition().getTotalVacation();
+        int usedVacation = user.getUsedVacation();
+
+        GetUserHeaderDTO getUserHeaderDTO = GetUserHeaderDTO.from(user, usedVacation, totalVacation);
+
+        return ResponseEntity.ok(ApiResponse.success(getUserHeaderDTO));
+    }
     @GetMapping("/api/v1/user/info")
     public ResponseEntity<ApiResponse.Result<UserResponse.GetMyPageDTO>> getMyPage() {
         Long userId = 1L;
