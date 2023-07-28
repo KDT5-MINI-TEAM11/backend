1d95f62333934544048089edab3570b0d00ea15d
jongy <jongyoon+u@gmail.com>
2023-07-28 23:13:58 +0900

    feat #2 : update Position
    Change Enum value
    add total vacation field

--- src/main/java/fastcampus/scheduling/user/common/Position.java
+++ src/main/java/fastcampus/scheduling/user/common/Position.java
@@ -1,13 +1,22 @@
 package fastcampus.scheduling.user.common;
 
 import java.util.Objects;
+import lombok.Getter;
 
+@Getter
 public enum Position {
-    //임시
-    INTERN,
-    STAFF,
-    ASSISTANT_MANAGER,
-    MANAGER;
+
+    LEVEL1(15),
+    LEVEL2(18),
+    LEVEL3(21),
+    LEVEL4(24),
+    MANAGER(27);
+
+    private final int totalVacation;
+
+    Position(int totalVacation) {
+        this.totalVacation = totalVacation;
+    }
 
     public static Position from(String name) {
         for (Position role : Position.values()) {
@@ -17,4 +26,5 @@ public enum Position {
         throw new RuntimeException();
         //throw new Exception500("권한 매칭 오류");
     }
+
 }
