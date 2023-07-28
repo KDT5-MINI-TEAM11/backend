5b65bd8e9e063f0e133ec69ed531309d0e1338a3
jongy <jongyoon+u@gmail.com>
2023-07-28 19:07:12 +0900

    feat #2 : updateDTO
    updateDTO

--- src/main/java/fastcampus/scheduling/user/dto/UserResponse.java
+++ src/main/java/fastcampus/scheduling/user/dto/UserResponse.java
@@ -1,10 +1,13 @@
 package fastcampus.scheduling.user.dto;
 
-import fastcampus.scheduling.user.user.model.User;
+import fastcampus.scheduling.user.common.Position;
+import fastcampus.scheduling.user.model.User;
 import lombok.Builder;
 import lombok.Getter;
 
 public class UserResponse {
+
+
 
     @Getter
     @Builder
@@ -18,6 +21,29 @@ public class UserResponse {
                 .userEmail(user.getUserEmail())
                 .build();
         }
+    }
+
+    @Getter
+    @Builder
+    public static class GetUserHeaderDTO {
+        private String userName;
+        private String userEmail;
+        private Position position;
+        private String profileThumbNail;
+        private int usedVacation;
+        private int totalVacation;
+
+        public static GetUserHeaderDTO from(User user, int usedVacation, int totalVacation) {
+            return GetUserHeaderDTO.builder()
+                .userName(user.getUserName())
+                .userEmail(user.getUserEmail())
+                .position(user.getPosition())
+                .profileThumbNail(user.getProfileThumbUrl())
+                .usedVacation(usedVacation)
+                .totalVacation(totalVacation)
+                .build();
+        }
+
     }
 
 }
