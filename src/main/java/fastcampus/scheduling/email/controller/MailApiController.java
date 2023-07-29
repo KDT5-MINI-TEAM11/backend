be8e6737b03dd13469f05f903746d384bd3ef921
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-29 20:41:50 +0900

    fix #9 : Mail관련 머지 충돌 코드 수정 후 주석 제거 및 DTO 위치 변경

--- src/main/java/fastcampus/scheduling/email/controller/MailApiController.java
+++ src/main/java/fastcampus/scheduling/email/controller/MailApiController.java
@@ -1,11 +1,10 @@
 package fastcampus.scheduling.email.controller;
 
-//import fastcampus.scheduling._core.util.ApiResponse;
-//import fastcampus.scheduling._core.util.ApiResponse.Result;
-//import fastcampus.scheduling.user.dto.UserRequest;
-//import fastcampus.scheduling.user.dto.UserRequest.SendEmailDTO;
-//import fastcampus.scheduling.user.dto.UserResponse;
-//import fastcampus.scheduling.user.dto.UserResponse.AuthEmail;
+import fastcampus.scheduling._core.util.ApiResponse;
+import fastcampus.scheduling._core.util.ApiResponse.Result;
+import fastcampus.scheduling.email.dto.EmailRequest;
+import fastcampus.scheduling.email.dto.EmailResponse;
+import fastcampus.scheduling.email.dto.EmailResponse.AuthEmailDTO;
 import fastcampus.scheduling.email.service.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,20 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailApiController {
 
     private final MailService mailService;
-//    @PostMapping("/api/v1/auth/checkEmail")
-//    public ResponseEntity<Result<Object>> checkEmail(@RequestBody UserRequest.CheckEmailDTO checkEmailDTO, Errors errors) {
-//        log.info("/api/v1/auth/checkEmail POST " + checkEmailDTO);
-//        mailService.checkEmail(checkEmailDTO);
-//
-//        return ResponseEntity.ok(ApiResponse.success(null));
-//    }
-//
-//    @PostMapping("/api/v1/auth/sendEmail")
-//    public ResponseEntity<ApiResponse.Result<UserResponse.AuthEmail>> sendEmail(@RequestBody SendEmailDTO sendEmailDTO, Errors errors) {
-//        log.info("/api/v1/auth/sendEmail POST " + sendEmailDTO);
-//        AuthEmail authEmail = mailService.sendEmail(sendEmailDTO);
-//
-//        return ResponseEntity.ok(ApiResponse.success(authEmail));
-//    }
+    @PostMapping("/api/v1/auth/checkEmail")
+    public ResponseEntity<Result<Object>> checkEmail(@RequestBody EmailRequest.CheckEmailDTO checkEmailDTO, Errors errors) {
+        log.info("/api/v1/auth/checkEmail POST " + checkEmailDTO);
+        mailService.checkEmail(checkEmailDTO);
+
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @PostMapping("/api/v1/auth/sendEmail")
+    public ResponseEntity<ApiResponse.Result<EmailResponse.AuthEmailDTO>> sendEmail(@RequestBody EmailRequest.SendEmailDTO sendEmailDTO, Errors errors) {
+        log.info("/api/v1/auth/sendEmail POST " + sendEmailDTO);
+        AuthEmailDTO authEmail = mailService.sendEmail(sendEmailDTO);
+
+        return ResponseEntity.ok(ApiResponse.success(authEmail));
+    }
 
 }
