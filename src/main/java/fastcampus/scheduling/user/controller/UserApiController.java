ab23bc587ac6572e5ed6813d621e7302b3758086
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-31 05:16:50 +0900

    feat #9 : user관련 valid, exception 추가

--- src/main/java/fastcampus/scheduling/user/controller/UserApiController.java
+++ src/main/java/fastcampus/scheduling/user/controller/UserApiController.java
@@ -4,6 +4,8 @@ import fastcampus.scheduling._core.util.ApiResponse;
 import fastcampus.scheduling.user.dto.UserRequest;
 import fastcampus.scheduling.user.dto.UserResponse;
 import fastcampus.scheduling.user.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,16 +22,15 @@ public class UserApiController {
     private final UserService userService;
 
     @PostMapping("/api/v1/auth/signup")
-    public ResponseEntity<ApiResponse.Result<UserResponse.SignUpDTO>> signUp(@RequestBody UserRequest.SignUpDTO signUpDTO, Errors errors) {
-
-        log.info("/api/user/signup POST " + signUpDTO);
-        UserResponse.SignUpDTO signUpResponse = userService.save(signUpDTO); // 중복등은 서비스에서 체크
+    public ResponseEntity<ApiResponse.Result<UserResponse.SignUpDTO>> signUp(HttpServletRequest request, @RequestBody @Valid UserRequest.SignUpDTO signUpDTO, Errors errors) {
+        log.info("/api/v1/auth/signup POST " + signUpDTO);
+        UserResponse.SignUpDTO signUpResponse = userService.save(request, signUpDTO); // 중복등은 서비스에서 체크
 
         return ResponseEntity.ok(ApiResponse.success(signUpResponse));
     }
 
     @PostMapping("/api/v1/auth/checkPhoneNumber")
-    public ResponseEntity<ApiResponse.Result<Object>> checkPhoneNumber(@RequestBody UserRequest.CheckPhoneDTO checkPhoneDTO, Errors erros) {
+    public ResponseEntity<ApiResponse.Result<Object>> checkPhoneNumber(@RequestBody @Valid UserRequest.CheckPhoneDTO checkPhoneDTO, Errors erros) {
         log.info("/api/v1/auth/checkPhoneNumber POST " + checkPhoneDTO);
         userService.checkPhone(checkPhoneDTO);
 
