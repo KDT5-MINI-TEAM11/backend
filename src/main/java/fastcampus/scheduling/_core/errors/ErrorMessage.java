e405dfef1cfd2f9c2fce87bf8af804f418551dec
GyuChangSeong <ahdzlq12@gmail.com>
2023-07-31 19:36:29 +0900

    feat #21 : 회원가입 email 인증번호 체크

--- src/main/java/fastcampus/scheduling/_core/errors/ErrorMessage.java
+++ src/main/java/fastcampus/scheduling/_core/errors/ErrorMessage.java
@@ -13,6 +13,7 @@ public class ErrorMessage {
     public static final String EMPTY_DATA_FOR_USER_SIGNUP = "회원 가입을 위한 데이터가 존재하지 않습니다.";
     public static final String EMPTY_DATA_FOR_USER_UPDATE = "회원 정보 수정을 위한 데이터가 존재하지 않습니다.";
     public static final String EMPTY_DATA_FOR_USER_CHECK_USEREMAIL = "이메일 중복 검사를 위한 데이터가 존재하지 않습니다.";
+    public static final String EMPTY_DATA_FOR_USER_CHECK_USEREMAILAUTH = "이메일 인증 검사를 위한 데이터가 존재하지 않습니다.";
     public static final String EMPTY_DATA_FOR_USER_CHECK_PhoneNumber = "핸드폰 번호 중복 검사를 위한 데이터가 존재하지 않습니다.";
     public static final String EMPTY_DATA_FOR_SAVE_CONTENT = "본문 내용을 입력해주세요.";
     public static final String EMPTY_DATA_FOR_SAVE_TITLE = "제목을 입력해주세요.";
@@ -25,6 +26,7 @@ public class ErrorMessage {
     public static final String INVALID_PASSWORD = "패스워드는 8자에서 16자 사이어야 합니다.";
     public static final String INVALID_EMAIL = "이메일 형식을 맞춰주세요.";
     public static final String INVALID_SEND_EMAIL = "인증 이메일이 잘못 되었습니다.";
+    public static final String INVALID_SEND_EMAILAUTH = "인증 번호가 잘못 되었습니다.";
     public static final String NOT_FOUND_USER_FOR_UPDATE = "회원 정보 수정을 위한 회원 정보가 존재하지 않습니다.";
     public static final String DUPLICATE_USEREMAIL = "이미 가입 된 이메일 입니다.";
     public static final String DUPLICATE_PHONENUMBER = "이미 저장 된 번호 입니다.";
