3b37db2aeddf20c1ff63a82047cce3b47bf6f29e
GyuChangSeong <ahdzlq12@gmail.com>
2023-08-02 19:46:24 +0900

    test : 로그인 로그 추가

--- src/main/java/com/fastcampus/scheduling/_core/security/filter/CustomAuthenticationProvider.java
+++ src/main/java/com/fastcampus/scheduling/_core/security/filter/CustomAuthenticationProvider.java
@@ -5,6 +5,7 @@ import static com.fastcampus.scheduling._core.errors.ErrorMessage.MISMATCH_SIGN_
 import com.fastcampus.scheduling.user.service.UserService;
 import javax.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationProvider implements org.springframework.security.authentication.AuthenticationProvider {
@@ -27,7 +29,10 @@ public class CustomAuthenticationProvider implements org.springframework.securit
         String userPassword = (String) authenticationToken.getCredentials();
 
         org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) userService.loadUserByUsername(userEmail);
+        log.info("authenticate: " + user.getUsername());
+        log.info("authenticate: " + user.getPassword());
         if (!passwordEncoder.matches(userPassword, user.getPassword())) {
+            log.warn("password Not Match");
             throw new BadCredentialsException(MISMATCH_SIGN_IN_INFO);
         }
         // set credentials null
