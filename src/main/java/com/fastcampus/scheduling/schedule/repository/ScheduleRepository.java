ee87444b78c4e71c1261bc08fc2d3aaf2128efea
GyuChangSeong <ahdzlq12@gmail.com>
2023-08-03 04:44:55 +0900

    feat #36 : 스케줄 데이터 타입 변경 및 Repository 메서드 추가

--- src/main/java/com/fastcampus/scheduling/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/fastcampus/scheduling/schedule/repository/ScheduleRepository.java
@@ -1,10 +1,11 @@
 package com.fastcampus.scheduling.schedule.repository;
 
 import com.fastcampus.scheduling.schedule.model.Schedule;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
+    Optional<Schedule> findByUserId(Long userId);
 }
