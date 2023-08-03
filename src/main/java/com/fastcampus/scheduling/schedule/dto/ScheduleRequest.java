fbb61f0dcdb3fd00a18ec3fb9a5ebbe0238ac7f3
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-08-03 16:41:54 +0900

    feat #34 : Schedule DTO 추가

--- src/main/java/com/fastcampus/scheduling/schedule/dto/ScheduleRequest.java
+++ src/main/java/com/fastcampus/scheduling/schedule/dto/ScheduleRequest.java
@@ -1,5 +1,36 @@
 package com.fastcampus.scheduling.schedule.dto;
 
+import com.fastcampus.scheduling.schedule.common.ScheduleType;
+import com.fastcampus.scheduling.schedule.common.State;
+import com.fastcampus.scheduling.schedule.model.Schedule;
+import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 public class ScheduleRequest {
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @ToString
+    public static class ModifyScheduleDTO {
+
+        @NotBlank
+        private LocalDate startDate;
+
+        @NotBlank
+        private LocalDate endDate;
+
+        public static ModifyScheduleDTO from(Schedule schedule) {
+            return ModifyScheduleDTO.builder()
+                .startDate(schedule.getStartDate())
+                .endDate(schedule.getEndDate())
+                .build();
+        }
+    }
 }
--- src/main/java/com/fastcampus/scheduling/schedule/dto/ScheduleResponse.java
+++ src/main/java/com/fastcampus/scheduling/schedule/dto/ScheduleResponse.java
@@ -3,12 +3,69 @@ package com.fastcampus.scheduling.schedule.dto;
 import com.fastcampus.scheduling.schedule.common.ScheduleType;
 import com.fastcampus.scheduling.schedule.common.State;
 import com.fastcampus.scheduling.schedule.model.DummySchedule;
+import com.fastcampus.scheduling.schedule.model.Schedule;
 import com.fastcampus.scheduling.user.model.DummyUser;
 import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 public class ScheduleResponse {
+
+	@Getter
+	@Builder
+	public static class GetUserScheduleDTO {
+
+		private ScheduleType scheduleType;
+
+		private State state;
+
+		private LocalDate startDate;
+
+		private LocalDate endDate;
+
+		public static GetUserScheduleDTO from(Schedule schedule) {
+			return GetUserScheduleDTO.builder()
+				.scheduleType(schedule.getScheduleType())
+				.state(schedule.getState())
+				.startDate(schedule.getStartDate())
+				.endDate(schedule.getEndDate())
+				.build();
+		}
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@ToString
+	public static class AddScheduleDTO {
+
+		@NotBlank
+		private ScheduleType scheduleType;
+
+		@NotBlank
+		private LocalDate startDate;
+
+		@NotBlank
+		private LocalDate endDate;
+
+		@NotBlank
+		private State state;
+
+		public static ScheduleResponse.AddScheduleDTO from(Schedule schedule) {
+			return ScheduleResponse.AddScheduleDTO.builder()
+				.scheduleType(schedule.getScheduleType())
+				.startDate(schedule.getStartDate())
+				.endDate(schedule.getEndDate())
+				.state(schedule.getState())
+				.build();
+		}
+	}
+
 	@Getter
 	@Builder
 	public static class GetScheduleDTO {
