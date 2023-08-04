73b841a9cadb8a45b08fe5ba22688a2dff2dd309
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-08-04 15:53:15 +0900

    feat #34 : Schedule 모든 연차/당직 확인(년/월)

--- src/main/java/com/fastcampus/scheduling/schedule/controller/ScheduleController.java
+++ src/main/java/com/fastcampus/scheduling/schedule/controller/ScheduleController.java
@@ -3,12 +3,14 @@ package com.fastcampus.scheduling.schedule.controller;
 import com.fastcampus.scheduling._core.util.ApiResponse;
 import com.fastcampus.scheduling._core.util.ApiResponse.Result;
 import com.fastcampus.scheduling.schedule.dto.ScheduleRequest.ModifyScheduleDTO;
-import com.fastcampus.scheduling.schedule.dto.ScheduleResponse;
 import com.fastcampus.scheduling.schedule.dto.ScheduleResponse.AddScheduleDTO;
+import com.fastcampus.scheduling.schedule.dto.ScheduleResponse.GetAllScheduleDTO;
 import com.fastcampus.scheduling.schedule.dto.ScheduleResponse.GetUserScheduleDTO;
 import com.fastcampus.scheduling.schedule.model.Schedule;
 import com.fastcampus.scheduling.schedule.service.ScheduleServiceImpl;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,20 +31,26 @@ public class ScheduleController {
     private final ScheduleServiceImpl scheduleServiceImpl;
 
     @GetMapping("/user/schedule")
-    public ResponseEntity<Result<ScheduleResponse.GetUserScheduleDTO>> getSchedule() {
+    public ResponseEntity<Result<List<GetUserScheduleDTO>>> getSchedule() {
         Long userId = Long.valueOf(
             SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
 
-        Schedule schedule = scheduleServiceImpl.getScheduleById(userId);
+        List<Schedule> schedules = scheduleServiceImpl.getAllSchedulesByUserId(userId);
 
-        GetUserScheduleDTO getUserScheduleDTO = GetUserScheduleDTO.from(schedule);
+        List<GetUserScheduleDTO> userAllSchedulesDTO = schedules.stream()
+            .map(schedule -> GetUserScheduleDTO.from(schedule))
+            .collect(Collectors.toList());
 
-        return ResponseEntity.ok(ApiResponse.success(getUserScheduleDTO));
+        return ResponseEntity.ok(ApiResponse.success(userAllSchedulesDTO));
 
     }
 
     @PostMapping("/user/schedule/add")
     public ResponseEntity<Result<AddScheduleDTO>>  addSchedule(@RequestBody AddScheduleDTO addScheduleDTO) {
+
+        Long userId = Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+
+        addScheduleDTO.setUserId(userId);
 
         Schedule savedSchedule = scheduleServiceImpl.addSchedule(addScheduleDTO);
 
@@ -57,29 +66,41 @@ public class ScheduleController {
 
     @PatchMapping("/user/schedule/modify")
     public ResponseEntity<Result<ModifyScheduleDTO>> modifySchedule(@RequestBody ModifyScheduleDTO modifyScheduleDTO) {
-
         Long userId = Long.valueOf(
             SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
 
-        Schedule schedule = scheduleServiceImpl.getScheduleById(userId);
+        Schedule existingSchedule = scheduleServiceImpl.getScheduleById(userId);
 
-        LocalDate startDate = modifyScheduleDTO.getStartDate();
-        if (startDate != null) {
-            schedule.setStartDate(startDate);
+        LocalDate newStartDate = modifyScheduleDTO.getStartDate();
+        LocalDate newEndDate = modifyScheduleDTO.getEndDate();
+
+        if (newStartDate != null) {
+            existingSchedule.setStartDate(newStartDate);
         }
 
-        LocalDate endDate = modifyScheduleDTO.getEndDate();
-        if (endDate != null) {
-            schedule.setEndDate(endDate);
+        if (newEndDate != null) {
+            existingSchedule.setEndDate(newEndDate);
         }
 
-        Schedule modifySchedule = scheduleServiceImpl.modifySchedule(userId, schedule.getStartDate(), schedule.getEndDate());
+        Schedule modifiedSchedule = scheduleServiceImpl.modifySchedule(userId, existingSchedule.getStartDate(), existingSchedule.getEndDate());
 
-        ModifyScheduleDTO modifyScheduleDTOs = ModifyScheduleDTO.from(modifySchedule);
+        ModifyScheduleDTO modifyScheduleResponseDTO = ModifyScheduleDTO.from(modifiedSchedule);
 
-        return ResponseEntity.ok(ApiResponse.success(modifyScheduleDTOs));
+        return ResponseEntity.ok(ApiResponse.success(modifyScheduleResponseDTO));
     }
 
+    @GetMapping("/user/schedule/list")
 
+    public ResponseEntity<Result<List<GetAllScheduleDTO>>> getAllSchedules(@RequestParam("year") int year, @RequestParam("month") int month) {
+
+        List<Schedule> allSchedules = scheduleServiceImpl.getSchedulesByYearAndMonth(year, month);
+
+        List<GetAllScheduleDTO> allSchedulesDTO = allSchedules.stream()
+            .map(schedule -> GetAllScheduleDTO.from(schedule))
+            .collect(Collectors.toList());
+
+        return ResponseEntity.ok(ApiResponse.success(allSchedulesDTO));
+
+    }
 
 }
--- src/main/java/com/fastcampus/scheduling/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/fastcampus/scheduling/schedule/repository/ScheduleRepository.java
@@ -1,8 +1,11 @@
 package com.fastcampus.scheduling.schedule.repository;
 
 import com.fastcampus.scheduling.schedule.model.Schedule;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +13,8 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Optional<Schedule> findByUserId(Long userId);
     List<Schedule> findAllByOrderByIdDesc();
 
+    List<Schedule> getAllSchedulesByUserId(Long userId);
+
+    @Query("SELECT s FROM Schedule s WHERE YEAR(s.startDate) = :year AND MONTH(s.startDate) = :month")
+    List<Schedule> findByStartDateYearAndStartDateMonth(@Param("year") int year, @Param("month") int month);
 }
