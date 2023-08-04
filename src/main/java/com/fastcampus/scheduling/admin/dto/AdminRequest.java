512ddd8ba9c975cd2dc6c6a9e44a2e69a0d0b6f3
GyuChangSeong <ahdzlq12@gmail.com>
2023-08-04 14:36:06 +0900

    feat #42 : 관리자페이지 기능 수정 및 추가
    변경사항
    -자신의 직책 수정 못하게 바꿈
    -관리자 update문 수정 -> 더티체킹
    -DTO수정

--- src/main/java/com/fastcampus/scheduling/admin/dto/AdminRequest.java
+++ src/main/java/com/fastcampus/scheduling/admin/dto/AdminRequest.java
@@ -1,10 +1,7 @@
 package com.fastcampus.scheduling.admin.dto;
 
-import com.fastcampus.scheduling.schedule.common.State;
 import com.fastcampus.scheduling.user.common.Position;
-import java.time.LocalDate;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,39 +14,33 @@ public class AdminRequest {
     @NoArgsConstructor
     @AllArgsConstructor
     @Getter
-    @Setter
     public static class ApproveDTO {
         @NotBlank
-        private Long userId;
-        @NotNull
-        private State state;
-        @NotBlank
-        private LocalDate startDate;
-        @NotBlank
-        private LocalDate endDate;
+        private Long id;
     }
 
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     @Getter
-    @Setter
     public static class RejectDTO {
         @NotBlank
-        private Long userId;
-        @NotNull
-        private State state;
-        @NotBlank
-        private LocalDate startDate;
-        @NotBlank
-        private LocalDate endDate;
+        private Long id;
     }
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class PendingDTO {
+        @NotBlank
+        private Long id;
+    }
+
 
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     @Getter
-    @Setter
     public static class UpdatePositionDTO{
         @NotBlank
         private Long id;
--- src/main/java/com/fastcampus/scheduling/admin/service/AdminService.java
+++ src/main/java/com/fastcampus/scheduling/admin/service/AdminService.java
@@ -4,15 +4,17 @@ import com.fastcampus.scheduling._core.errors.ErrorMessage;
 import com.fastcampus.scheduling._core.errors.exception.Exception400;
 import com.fastcampus.scheduling.admin.dto.AdminRequest;
 import com.fastcampus.scheduling.admin.dto.AdminRequest.ApproveDTO;
+import com.fastcampus.scheduling.admin.dto.AdminRequest.PendingDTO;
 import com.fastcampus.scheduling.admin.dto.AdminRequest.RejectDTO;
 import com.fastcampus.scheduling.admin.dto.AdminResponse;
 import com.fastcampus.scheduling.admin.dto.AdminResponse.GetAllScheduleDTO;
+import com.fastcampus.scheduling.admin.dto.AdminResponse.GetAllUserDTO;
 import com.fastcampus.scheduling.schedule.common.State;
 import com.fastcampus.scheduling.schedule.model.Schedule;
 import com.fastcampus.scheduling.schedule.repository.ScheduleRepository;
+import com.fastcampus.scheduling.user.common.Position;
 import com.fastcampus.scheduling.user.model.User;
 import com.fastcampus.scheduling.user.repository.UserRepository;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -26,8 +28,8 @@ public class AdminService {
     private final ScheduleRepository scheduleRepository;
     private final UserRepository userRepository;
 
-    public List<AdminResponse.GetAllScheduleDTO> findAll(){
-        List<Schedule> scheduleList = scheduleRepository.findAll();
+    public List<AdminResponse.GetAllScheduleDTO> findAllSchedule(){
+        List<Schedule> scheduleList = scheduleRepository.findAllByOrderByIdDesc();
         List<AdminResponse.GetAllScheduleDTO> getAllScheduleDTOList = new ArrayList<>();
         for (Schedule schedule : scheduleList) {
             getAllScheduleDTOList.add(GetAllScheduleDTO.builder()
@@ -43,76 +45,61 @@ public class AdminService {
         return getAllScheduleDTOList;
     }
 
-    @Transactional
-    public AdminResponse.ResolveDTO updateScheduleReject(RejectDTO rejectDTO){
-        Optional<Schedule> scheduleOptional = scheduleRepository.findByUserId(rejectDTO.getUserId());
-        Optional<User> userOptional = userRepository.findById(rejectDTO.getUserId());
-
-        if(!scheduleOptional.isPresent())
-            throw new Exception400(ErrorMessage.EMPTY_DATA_FOR_SCHEDULE);
-
-        Schedule schedule = scheduleRepository.save(Schedule.builder()
-            .id(scheduleOptional.get().getId())
-            .user(userOptional.get())
-            .scheduleType(scheduleOptional.get().getScheduleType())
-            .state(State.REJECT)
-            .startDate(rejectDTO.getStartDate())
-            .endDate(rejectDTO.getEndDate())
-            .createdAt(scheduleOptional.get().getCreatedAt())
-            .updatedAt(LocalDate.now())
-            .build());
-
-        return AdminResponse.ResolveDTO.builder()
-            .scheduleType(schedule.getScheduleType())
-            .state(schedule.getState())
-            .build();
+    @Transactional(readOnly = true)
+    public List<AdminResponse.GetAllUserDTO> findAllUser(){
+        List<User> userList = userRepository.findAllByOrderByIdDesc();
+        List<AdminResponse.GetAllUserDTO> getAllUserDTOList = new ArrayList<>();
+        for (User user : userList) {
+            getAllUserDTOList.add(GetAllUserDTO.builder()
+                .id(user.getId())
+                .userName(user.getUserName())
+                .profileThumbUrl(user.getProfileThumbUrl())
+                .position(user.getPosition())
+                .createAt(user.getCreatedAt())
+                .build());
+        }
+        return getAllUserDTOList;
     }
 
     @Transactional
-    public AdminResponse.ResolveDTO updateScheduleApprove(ApproveDTO approveDTO){
-        Optional<Schedule> scheduleOptional = scheduleRepository.findByUserId(approveDTO.getUserId());
-        Optional<User> userOptional = userRepository.findById(approveDTO.getUserId());
+    public void updateScheduleReject(RejectDTO rejectDTO){
+        Optional<Schedule> scheduleOptional = scheduleRepository.findById(rejectDTO.getId());
 
-        if(!scheduleOptional.isPresent())
-            throw new Exception400(ErrorMessage.EMPTY_DATA_FOR_SCHEDULE);
+        scheduleOptional.orElseThrow(() -> new Exception400(ErrorMessage.EMPTY_DATA_FOR_SCHEDULE))
+            .setState(State.REJECT);
+    }
 
-        Schedule schedule = scheduleRepository.save(Schedule.builder()
-            .id(scheduleOptional.get().getId())
-            .user(userOptional.get())
-            .scheduleType(scheduleOptional.get().getScheduleType())
-            .state(State.APPROVE)
-            .startDate(approveDTO.getStartDate())
-            .endDate(approveDTO.getEndDate())
-            .createdAt(scheduleOptional.get().getCreatedAt())
-            .updatedAt(LocalDate.now())
-            .build());
+    @Transactional
+    public void updateScheduleApprove(ApproveDTO approveDTO){
+        Optional<Schedule> scheduleOptional = scheduleRepository.findById(approveDTO.getId());
 
-        return AdminResponse.ResolveDTO.builder()
-            .scheduleType(schedule.getScheduleType())
-            .state(schedule.getState())
-            .build();
+        scheduleOptional.orElseThrow(() -> new Exception400(ErrorMessage.EMPTY_DATA_FOR_SCHEDULE))
+            .setState(State.APPROVE);
+    }
+
+    @Transactional
+    public void updateSchedulePending(PendingDTO pendingDTO){
+        Optional<Schedule> scheduleOptional = scheduleRepository.findById(pendingDTO.getId());
+
+        scheduleOptional.orElseThrow(() -> new Exception400(ErrorMessage.EMPTY_DATA_FOR_SCHEDULE))
+            .setState(State.PENDING);
     }
 
     @Transactional
     public void updatePosition(AdminRequest.UpdatePositionDTO updatePositionDTO){
         Optional<User> userOptional = userRepository.findById(updatePositionDTO.getId());
 
-        if(!userOptional.isPresent())
-            throw new Exception400(ErrorMessage.EMPTY_DATA_FOR_SAVE_USER);
-
-        if(userOptional.orElseThrow().getPosition() == null)
+        if(userOptional.orElseThrow(() -> new Exception400(ErrorMessage.EMPTY_DATA_FOR_SAVE_USER))
+            .getPosition() == null)
             throw new Exception400(ErrorMessage.EMPTY_DATA_FOR_USER_CHECK_POSITION);
 
-        userRepository.save(User.builder()
-            .id(userOptional.get().getId())
-            .userEmail(userOptional.get().getUserEmail())
-            .userPassword(userOptional.get().getUserPassword())
-            .userName(userOptional.get().getUserName())
-            .profileThumbUrl(userOptional.get().getProfileThumbUrl())
-            .position(updatePositionDTO.getPosition())
-            .phoneNumber(userOptional.get().getPhoneNumber())
-            .usedVacation(userOptional.get().getUsedVacation())
-            .createdAt(userOptional.get().getCreatedAt())
-            .build());
+        userOptional.orElseThrow(()-> new Exception400(ErrorMessage.EMPTY_DATA_FOR_SAVE_USER))
+            .setPosition(updatePositionDTO.getPosition());
+    }
+
+    public Position getPosition(Long userId){
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        return userOptional.orElseThrow().getPosition();
     }
 }
