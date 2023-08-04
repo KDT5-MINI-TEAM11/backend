4566c2baa30ddd73a307f7fc65b74879b6903e3a
GyuChangSeong <ahdzlq12@gmail.com>
2023-08-04 14:45:22 +0900

    feat #42 : 내림차순 및 에러메시지 추가, 변수타입 변경
    TimeStamp -> LocalDate 변경

--- src/main/java/com/fastcampus/scheduling/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/fastcampus/scheduling/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,7 @@
 package com.fastcampus.scheduling.schedule.repository;
 
 import com.fastcampus.scheduling.schedule.model.Schedule;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Optional<Schedule> findByUserId(Long userId);
+    List<Schedule> findAllByOrderByIdDesc();
 }
