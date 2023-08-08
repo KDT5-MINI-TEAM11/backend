f14dbdb861da3b04e23d13108e33bdac4db05e78
jongy <jongyoon+u@gmail.com>
2023-08-09 07:18:17 +0900

    feat #60 : add batch
    change schedule's state pending to reject if over date

---
+++ src/main/java/com/fastcampus/scheduling/_core/batch/DayChangeScheduler.java
@@ -0,0 +1,40 @@
+package com.fastcampus.scheduling._core.batch;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DayChangeScheduler {
+
+	private final ScheduleCursorJobConfiguration scheduleCursorJobConfiguration;
+	private final JobLauncher jobLauncher;
+
+	@Scheduled(cron = "0 0 0 * * *")
+	public void run() {
+		// job parameter 설정
+		Map<String, JobParameter> confMap = new HashMap<>();
+		confMap.put("time", new JobParameter(System.currentTimeMillis()));
+		JobParameters jobParameters = new JobParameters(confMap);
+
+		try {
+			jobLauncher.run(scheduleCursorJobConfiguration.schedulePagingJob(), jobParameters);
+		} catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
+						 | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
+
+			log.error(e.getMessage());
+		}
+	}
+
+}
