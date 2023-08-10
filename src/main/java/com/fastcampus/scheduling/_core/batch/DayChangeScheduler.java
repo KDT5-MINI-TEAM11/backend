dce29dd355dbd07da50a64179617092dbe1a4599
jongy <jongyoon+u@gmail.com>
2023-08-10 19:09:35 +0900

    Feat #79 : init used vacation
    add batch scheduler for init used vacation every jan. first

--- src/main/java/com/fastcampus/scheduling/_core/batch/DayChangeScheduler.java
+++ src/main/java/com/fastcampus/scheduling/_core/batch/DayChangeScheduler.java
@@ -19,10 +19,11 @@ import org.springframework.stereotype.Component;
 public class DayChangeScheduler {
 
 	private final ScheduleJobConfiguration scheduleJobConfiguration;
+	private final InitUsedVacationJobConfiguration initUsedVacationJobConfiguration;
 	private final JobLauncher jobLauncher;
 
 	@Scheduled(cron = "0 0 0 * * *")
-	public void run() {
+	public void scheduleStateChangeRun() {
 		// job parameter 설정
 		Map<String, JobParameter> confMap = new HashMap<>();
 		confMap.put("time", new JobParameter(System.currentTimeMillis()));
@@ -30,6 +31,22 @@ public class DayChangeScheduler {
 
 		try {
 			jobLauncher.run(scheduleJobConfiguration.schedulePagingJob(), jobParameters);
+		} catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
+						 | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
+
+			log.error(e.getMessage());
+		}
+	}
+
+	@Scheduled(cron = "0 0 0 1 1 ?")
+	public void initUsedVacationRun() {
+		// job parameter 설정
+		Map<String, JobParameter> confMap = new HashMap<>();
+		confMap.put("time", new JobParameter(System.currentTimeMillis()));
+		JobParameters jobParameters = new JobParameters(confMap);
+
+		try {
+			jobLauncher.run(initUsedVacationJobConfiguration.initUsedVacationPagingJob(), jobParameters);
 		} catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
 						 | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
 
