e1bf4c983c07dddc5cbc0d27de85b755014eb966
jongy <jongyoon+u@gmail.com>
2023-08-21 02:26:49 +0900

    style : Remove useless class

--- src/main/java/com/fastcampus/scheduling/_core/security/filter/CustomAuthenticationProvider.java
+++ src/main/java/com/fastcampus/scheduling/_core/security/filter/CustomAuthenticationProvider.java
@@ -4,7 +4,6 @@ import static com.fastcampus.scheduling._core.errors.ErrorMessage.MISMATCH_SIGN_
 
 import com.fastcampus.scheduling._core.errors.exception.Exception400;
 import com.fastcampus.scheduling.user.service.UserService;
-import java.nio.charset.StandardCharsets;
 import javax.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,8 +30,6 @@ public class CustomAuthenticationProvider implements org.springframework.securit
 
         org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) userService.loadUserByUsername(userEmail);
 
-        byte[] passworDb = userPassword.getBytes(StandardCharsets.UTF_8);
-        CustomBCrypt.checkpw(passworDb, user.getPassword());
         if (!passwordEncoder.matches(userPassword, user.getPassword())) {
             log.warn("password Not Match From matches");
             throw new Exception400(MISMATCH_SIGN_IN_INFO);
